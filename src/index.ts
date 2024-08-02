2a2fa9e242fff11fb887119ee7e003caa02635ac
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-08-02 19:07:13 +0530

    adding middleware

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,30 @@
 
 // export default app
 
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.post('/', async (c) => {
-  const body = await c.req.json()
+async function authMiddleware(c: any, next: any) {
+  // c stands for context of this request, response
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next();
+  } else {
+    return c.text("You dont have acces");
+  }
+}
+// app.use(authMiddleware);
+
+app.post("/", authMiddleware, async (c) => {
+  const body = await c.req.json();
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
 
-  return c.text('Hello Hono!')
-})
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
 
 //  for more examples of everything https://developers.cloudflare.com/workers/examples/
